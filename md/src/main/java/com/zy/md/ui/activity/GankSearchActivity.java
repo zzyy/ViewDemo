d9d7f90e7512895e266b6c6524013c2dc2cb4867
Simon <zzyy.alex@gmail.com>
2016-12-15 00:08:40 +0800

    2016-12-15 00:08:33

--- md/src/main/java/com/zy/md/ui/activity/GankSearchActivity.java
+++ md/src/main/java/com/zy/md/ui/activity/GankSearchActivity.java
@@ -2,11 +2,16 @@ package com.zy.md.ui.activity;
 
 import android.animation.Animator;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.transition.Fade;
+import android.transition.Transition;
+import android.transition.Visibility;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 
 import com.zy.md.R;
 import com.zy.md.base.view.BaseActivity;
+import com.zy.md.utils.common.MathUtils;
 
 import butterknife.BindView;
 
@@ -25,16 +30,59 @@ public class GankSearchActivity extends BaseActivity {
     }
 
     private void setupTransitionAnimator() {
-        int width = mSearchContainerView.getWidth();
-        int height = mSearchContainerView.getHeight();
 
-        final float maxRadius = (float) Math.sqrt( width*width + height+height  );
+//        Transition shareTransition = getWindow().getSharedElementEnterTransition();
+
+        Visibility enterTransition = buildEnterTransition();
+        getWindow().setEnterTransition( enterTransition );
+//        Transition enterTransition = getWindow().getEnterTransition();
+        enterTransition.addListener(new Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+                playEnterRevealAnimator();
+
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                enterTransition.removeListener(this);
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionPause(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionResume(Transition transition) {
+
+            }
+        });
+
+
+
+    }
+
+    private void playEnterRevealAnimator(){
+        final float maxRadius = MathUtils.calculateCatercorner(mSearchContainerView.getWidth(), mSearchContainerView.getHeight());
 
         Animator animator = ViewAnimationUtils.createCircularReveal(mSearchContainerView
-        , 0, 0, 0, maxRadius);
+                , 0, 0, 0, maxRadius);
         animator.setDuration(3000);
 
         animator.start();
+    }
 
+    private Visibility buildEnterTransition() {
+        Fade enterTransition = new Fade();
+        enterTransition.setDuration(1000);
+        enterTransition.excludeTarget(android.R.id.statusBarBackground, true);
+        enterTransition.excludeTarget(android.R.id.navigationBarBackground, true);
+        return enterTransition;
     }
 }
